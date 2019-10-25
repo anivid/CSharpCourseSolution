0126131ca2749601dc02a3da1c0188c361e3ccf4
anivid <matt_sok@mail.ru>
2019-10-25 17:25:59 +0700

    add TryDivide method to Cals

--- D_OOP/Program.cs
+++ D_OOP/Program.cs
@@ -10,10 +10,14 @@ namespace D_OOP
             //c.Hit(120);
             //Console.WriteLine(c.Heath);
             Calculator calc = new Calculator();
-            double s = calc.TriangleSquare(1,1,1,true);
+            double s = calc.TriangleSquare(1, 1, 1.0);
 
+            //именованые аргументы, один из вариантов передачи аргументов методу
+            double s1 = calc.TriangleSquare(a: 1, b: 2, angleAB: 3);
 
-            Console.WriteLine(s);
+            double[] arr = { 1, 2, 3, 4, 5 };
+            double avg = calc.Average(arr);
+            Console.WriteLine(avg);
         }   
     }
 }
--- D_OOP/Сalc.cs
+++ D_OOP/Сalc.cs
@@ -6,18 +6,29 @@ namespace D_OOP
 {
     public class Calculator
     {
-        public double TriangleSquare(double a, double b, double c)
+        //params позволяет при вызове метода заполнять массив элементами через запятую
+        //например calc.Average(1, 2, 3, 4, 5); будет передан массив items{1, 2, 3, 4, 5}
+        //так же можно передавать массив сразу целиком
+        public double Average(params double[] items)
         {
-            double p = (a + b + c) / 2;
-            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+            double summ = 0;
+            foreach (var number in items)
+            {
+                summ += number;
+            }
+            return summ/items.Length;
         }
-
-        public double TriangleSquare(double a, double h)
+        public double TriangleSquare(double sideA, double sideB, double sideC)
         {
-            return 0.5 * a * h;
+            double p = (sideA + sideB + sideC) / 2;
+            return Math.Sqrt(p * (p - sideA) * (p - sideB) * (p - sideC));
         }
-
-        public double TriangleSquare(double a, double b, double angleAB, bool isInRadiands)
+        public double TriangleSquare(double foot, double height)
+        {
+            return 0.5 * foot * height;
+        }
+        //bool isInRadiands = false - опциональный параметр
+        public double TriangleSquare(double a, double b, double angleAB, bool isInRadiands = false)
         {
             if (isInRadiands)
             {
@@ -28,6 +39,17 @@ namespace D_OOP
                 double angleRad = Math.PI / 180 * angleAB;
                 return 0.5 * a * b * Math.Sin(angleRad);
             }           
+        }
+        public bool TryDivide(double divider, double dividend, out double result)
+        {
+            //выходному значению обязательно нужно присвоить значение по умолчанию
+            result = 0;
+            if (dividend == 0)
+            {
+                return false;
+            }
+            result = divider / dividend;
+            return true;            
         }
     }
 }
