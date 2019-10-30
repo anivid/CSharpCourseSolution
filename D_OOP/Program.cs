d2f744d348a183e52fa2a75eedb9a6a6e4246ffd
anivid <matt_sok@mail.ru>
2019-10-30 18:00:56 +0700

    learn value and ref types as arguments in methods, and nullable types

--- D_OOP/Program.cs
+++ D_OOP/Program.cs
@@ -7,18 +7,77 @@ namespace D_OOP
     {
         static void Main(string[] args)
         {
+            
+        }
+
+        static void NRE_NullableValTypesDemo()
+        {
+            //PointVal pv; //присвоить null невозможно
+
+            //nullable PointVal type 
+            PointVal? pv = null;
+            if (pv.HasValue) //если не null
+            {
+                PointVal pv2 = pv.Value;
+                Console.WriteLine(pv.Value.X);//если не использовать if, будет NullReferenceException
+                //или
+                Console.WriteLine(pv2.X);
+            }
+
+            //если pv3 == null, то мы получим экземпляр по умолчанию
+            PointVal pv3 = pv.GetValueOrDefault();
+
+
+            //мы не выделили память для с в куче
+            PointRef c = null;
+            Console.WriteLine(c.X); //NullReferenceException (ссылка объекта не указывает на экземляр объекта)
+
+        }
+        static void ValRefTypesAsArguments()
+        {
+            int a = 1;
+            int b = 2;
+
+            //
+            Console.WriteLine("Main:\nпередаем в Swap(int a, int b) value types, т.е. значения а не ссылки");
+            Swap(a, b);
+            Console.WriteLine($"Main:\na = {a} b = {b}");
+            //передаем valye types по ссылке
+            Console.WriteLine("Main:\nпередаем в Swap(ref int a, ref int b) ref types, т.е. ссылки");
+            Swap(ref a, ref b);
+            Console.WriteLine($"Main:\na = {a} b = {b}");
+
+            Console.ReadLine();
             var list = new List<int>();
+            //передаем класс в качестве арумента
+            //копируется ссылка на list
             AddNumbers(list);
 
             foreach (var item in list)
             {
-                Console.WriteLine(item);
+                Console.WriteLine(item); //1 2 3
             }
         }
-
         static void Swap(ref int a, ref int b)
         {
-            
+            //для приема ссылочных значений
+            Console.WriteLine($"void Swap():\noriginal: a = {a}, b = {b}");
+            int tmp = a;
+            a = b;
+            b = tmp;
+
+            Console.WriteLine($"swapped: a = {a}, b = {b}\n");
+
+        }
+        static void Swap(int a, int b)
+        {
+            Console.WriteLine($"original: a = {a}, b = {b}");
+            int tmp = a;
+            a = b;
+            b = tmp;
+
+            Console.WriteLine($"swapped: a = {a}, b = {b}");
+
         }
 
         static void AddNumbers(List<int> numbers)
