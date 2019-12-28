b828ce795a8543d3f5b0a2676440c1d1b4533c24
anivid <matt_sok@mail.ru>
2019-12-28 11:00:15 +0700

    start learn FileStream

--- E_Exceptions/Program.cs
+++ E_Exceptions/Program.cs
@@ -1,18 +1,61 @@
 ﻿using System;
 using System.IO;
+using System.Text;
 
-namespace E_Exceptions
+namespace E_Filestream_and_Exceptions
 {
     class Program
     {
         static void Main(string[] args)
+        {
+            //с потоками ввода/вывода можно работать через класс Stream
+
+            Stream fs = new FileStream(@"test.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+
+            try
+            {
+                string str = "Hello, World";
+                //метод Write принимает буффер байтов, поэтому строку надо преобразовать в массив байт
+                byte[] strInBytes = Encoding.UTF8.GetBytes(str);
+
+                //в аргументы указываем массив байтов, начальный байт, конечный байт 
+                fs.Write(strInBytes, 0, strInBytes.Length);
+                //if (fs.CanWrite)
+                //{
+                //    fs.Write()
+                //}
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.ToString()}");
+            }
+            finally
+            {
+                fs.Close();
+            }
+
+            //при работе с классами, которые реализуют IDisposable, например FileStream
+            //конструкцию try-finally можно заменить на using
+            using (Stream readingStream = new FileStream("test.txt", FileMode.Open, FileAccess.Read))
+            {
+                byte[] temp = new byte[14];
+                int len = 0;
+                while((len = readingStream.Read(temp, 0, temp.Length)) > 0)
+                {
+                    string str = Encoding.UTF8.GetString(temp, 0, len);
+                    Console.WriteLine(str);
+                }
+            }
+        }
+        
+        static void ExtensionsDemo()
         {
             FileStream file = null;
             try
             {
                 file = File.Open("temp.txt", FileMode.Open);
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
             }
             finally
@@ -31,7 +74,7 @@ namespace E_Exceptions
             {
                 int number = int.Parse(result);
             }
-            catch(FormatException ex)
+            catch (FormatException ex)
             {
                 log = String.Concat("Exception catched", "\n", ex.ToString());
             }
